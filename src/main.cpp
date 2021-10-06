f6ff107163e8951737170ce049785c081403dd05
Ivan <Ivan@DESKTOP-0I6M0OS>
2021-10-06 11:18:58 +0300

    update parsing methods for parser shp config

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <iterator> 
 #include <algorithm>
 
+#include <sstream>
+
 #include <iostream>
 #include <windows.h>
 #include <atlstr.h>
@@ -12,6 +14,32 @@ const static char START_SECTION_DELIM = '[';
 const static char END_SECTION_DELIM = ']';
 const static char DOUBLE_QUOTE_DELIM = '\"';
 const static char DOT_DELIM = '.';
+
+const static long PRECISION_DOUBLE_REPR = 15;
+
+// убирает разделители в начале строки
+inline bool trim_delimeter_on_start(std::string& double_str_reps)
+{
+    try
+    {
+        if (double_str_reps[0] == COMMA_DELIM)
+        {
+            for (size_t k = 0; k < double_str_reps.size(); k++)
+            {
+                if (double_str_reps[0] != COMMA_DELIM)
+                    break;
+                else if (double_str_reps[0] == COMMA_DELIM)
+                    double_str_reps.erase(0, 1);
+            }
+        }
+    }
+    catch(...)
+    {
+        return false;
+    }
+
+    return true;
+}
 
 struct sectionValues
 {
@@ -33,8 +61,17 @@ struct SHPConfigPrj
     std::string initial_config;
 
     // обновляет значение для переданной секции
-    void update_section_value_by_section_name(const std::string& section_name, const std::string& val_for_replace)
+    bool update_section_value_by_section_name(const std::string& section_name, const std::string& val_for_replace)
     {
+        bool is_success = false;
+
+        if (section_name.size() == 0 || val_for_replace.size() == 0)
+            return is_success;
+
+        const std::size_t section_name_idx = initial_config.find(section_name);
+        if (section_name_idx == std::string::npos)
+            return is_success;
+
         std::string value_to_replace;
         for (std::pair<std::string, sectionValues>& section_info : data)
         {
@@ -46,37 +83,220 @@ struct SHPConfigPrj
             }
         }
 
+        if (value_to_replace.size() == 0)
+            return is_success;
+
         const size_t section_pos = initial_config.find(value_to_replace);
         const size_t section_value_to_replace_pos = initial_config.find(value_to_replace);
 
         if (section_value_to_replace_pos != std::string::npos)
+        {
+            is_success = true;
             initial_config.replace(section_pos, value_to_replace.length(), val_for_replace);
+        }
+
+        return is_success;
     }
 
-    // обновляет список значений секции
-    void update_values_by_section_name(const std::string& section_name, const std::vector<double>& values)
+    // считывает индексы подстрок
+    void parse_all_value_entries(std::vector<size_t>& vec, std::string data, std::string toSearch)
     {
-        //TODO
+        size_t pos = data.find(toSearch);
+        while (pos != std::string::npos)
+        {
+            vec.push_back(pos);
+            pos = data.find(toSearch, pos + toSearch.size());
+        }
     }
 
-    // обновляет конкретное значение из списка значений секции по переданному индексу
-    void update_section_values_by_section_name_and_idx(const std::string& section_name, const std::string& val_for_replace)
+    // в методе происходит замена значений списка параметров исходной секции на переданный
+    bool replace_section_value_by_index(const std::string& _double_str_reps, const std::vector<double>& values, const size_t idx)
     {
+        bool is_success = false;
+        // если строка не преобразуется - создастся исключение, и подстрока будет пропущена и last_delim_pos обновится
+        const double parsed_test_double = std::stod(_double_str_reps);
 
-        for (std::pair<std::string, sectionValues>& section_info : data)
+        std::ostringstream ss_double_repr;
+        ss_double_repr.precision(PRECISION_DOUBLE_REPR);
+        ss_double_repr << values[idx];
+        std::string value_to_replace = ss_double_repr.str();
+
+        std::vector<size_t> substr_indexes;
+        // де факто - проверка на уникальность вхождения, 
+        // могут быть случаи когда новое значение для записи - есть как подстрока одного из списка значений секции
+        // первое значение всегда ищется в исходной строке, далее уже в обновленной
+        parse_all_value_entries(substr_indexes, initial_config, _double_str_reps);
+
+        size_t start_pos_to_replace = 0;
+        if (values.size() == 1 && substr_indexes.size() >= 1)
+            start_pos_to_replace = substr_indexes[0];
+        else
+            start_pos_to_replace = substr_indexes.back();
+
+        const size_t section_pos = initial_config.find(_double_str_reps);
+
+        if (section_pos != std::string::npos)
+            initial_config.erase(start_pos_to_replace, _double_str_reps.length());
+        else
+            return is_success;
+
+        initial_config.insert(start_pos_to_replace, value_to_replace);
+
+        is_success = true;
+        return is_success;
+    }
+
+    // обновляет список значений секции, секция должна быть уникальной без дубликатов
+    // если секций несколько, то значение будет заменено для первой
+    // размерность переданного вектора значений и текущее количество значений секции должна быть одинаковой
+    bool update_values_by_section_name(const std::string& section_name, const std::vector<double>& values)
+    {
+        bool is_success = false;
+
+        if (section_name.size() == 0 || values.size() == 0)
+            return is_success;
+
+        const std::size_t section_name_idx = initial_config.find(section_name);
+        if (section_name_idx == std::string::npos)
+            return is_success;
+
+        std::vector<double> section_values;
+        std::string section_param;
+        for (const std::pair<std::string, sectionValues>& section_info : data)
         {
             if (section_info.second.section_name == section_name)
             {
-                section_info.second.section_value = val_for_replace;
+                section_values = section_info.second.section_values;
+                section_param = section_info.second.section_value;
                 break;
             }
         }
 
-        size_t section_pos = initial_config.find(section_name);
+        if(section_values.size() != values.size())
+            return is_success;
 
-        //TODO
-        // проверить выходит ли индекс за пределы списка
-        // считать значения, запомнить индексы, заменить
+        section_values.clear();
+        std::copy(values.begin(), values.end(), back_inserter(section_values));
+
+        const size_t section_values_end_pos = initial_config.find(']', section_name_idx + section_name.size() - 1);
+        const size_t start_slice_idx = ((section_name_idx + section_name.size() + 1) + section_param.size() + 1) + 1;
+        const size_t slice_chars_count = section_values_end_pos - start_slice_idx;
+
+        if (section_values_end_pos == std::string::npos || start_slice_idx == std::string::npos || slice_chars_count == 0)
+            return is_success;
+
+        // формируем список значений с разделителем ',' в виде строки
+        std::string initial_bind_str = initial_config.substr(start_slice_idx, slice_chars_count);
+        initial_bind_str += ',';
+
+        // для подстроки (значения) в текущем конфиге, ищем индексы и заменяем текущий срез для последнего найденного индекса искомой подстроки 
+        size_t values_replaced_count = 0;
+        size_t last_delim_pos = 0;
+        for (size_t l = 0; l < initial_bind_str.size(); l++)
+        {
+            if (initial_bind_str[l] == ',' && l > 0)
+            {
+                try
+                {
+                    std::string _double_str_reps = initial_bind_str.substr(last_delim_pos, l - last_delim_pos);
+
+                    if (!trim_delimeter_on_start(_double_str_reps) || _double_str_reps.size() == 0)
+                        continue;
+
+                    replace_section_value_by_index(_double_str_reps, values, values_replaced_count);
+
+                    values_replaced_count += 1;
+                }
+                catch (...)
+                {
+                    continue;
+                }
+
+                last_delim_pos = l;
+            }
+        }
+
+        is_success = true;
+        return is_success;
+    }
+
+    // обновляет конкретное значение из списка значений секции по переданному индексу
+    // индекс должен начинаться с 0
+    bool update_section_value_by_index(const std::string& section_name, const double& value, const size_t index)
+    {
+        bool is_success = false;
+
+        if (section_name.size() == 0)
+            return is_success;
+
+        const std::size_t section_name_idx = initial_config.find(section_name);
+        if (section_name_idx == std::string::npos)
+            return is_success;
+
+        std::vector<double> section_values;
+        std::string section_param;
+        for (const std::pair<std::string, sectionValues>& section_info : data)
+        {
+            if (section_info.second.section_name == section_name)
+            {
+                section_values = section_info.second.section_values;
+                section_param = section_info.second.section_value;
+                break;
+            }
+        }
+
+        if (index > section_values.size())
+            return is_success;
+
+        section_values[index] = value;
+
+        const size_t section_values_end_pos = initial_config.find(']', section_name_idx + section_name.size() - 1);
+        const size_t start_slice_idx = ((section_name_idx + section_name.size() + 1) + section_param.size() + 1) + 1;
+        const size_t slice_chars_count = section_values_end_pos - start_slice_idx;
+
+        if (section_values_end_pos == std::string::npos || start_slice_idx == std::string::npos || slice_chars_count == 0)
+            return is_success;
+
+        // формируем список значений с разделителем ',' в виде строки
+        std::string initial_bind_str = initial_config.substr(start_slice_idx, slice_chars_count);
+        initial_bind_str += ',';
+
+        // для подстроки (значения) в текущем конфиге, ищем индексы и заменяем текущий срез для последнего найденного индекса искомой подстроки 
+        size_t values_replaced_count = 0;
+        size_t last_delim_pos = 0;
+
+        std::vector<double> values;
+        values.push_back(value);
+        for (size_t l = 0; l < initial_bind_str.size(); l++)
+        {
+            if (initial_bind_str[l] == ',' && l > 0)
+            {
+                try
+                {
+                    std::string _double_str_reps = initial_bind_str.substr(last_delim_pos, l - last_delim_pos);
+
+                    if (!trim_delimeter_on_start(_double_str_reps) || _double_str_reps.size() == 0)
+                        continue;
+
+                    if (index == values_replaced_count)
+                    {
+                        replace_section_value_by_index(_double_str_reps, values, 0);
+                        break;
+                    }
+
+                    values_replaced_count += 1;
+                }
+                catch (...)
+                {
+                    continue;
+                }
+
+                last_delim_pos = l;
+            }
+        }
+
+        is_success = true;
+        return is_success;
     }
 };
 
@@ -104,19 +324,12 @@ inline void parse_values(const std::string& section_values, std::vector<double>&
             {
                 std::string _double_str_reps = section_values.substr(last_delim_pos, l - last_delim_pos);
 
-                if (_double_str_reps[0] == ',')
-                {
-                    for (size_t k = 0; k < _double_str_reps.size(); k++)
-                    {
-                        if (_double_str_reps[0] != ',')
-                            break;
-                        else if(_double_str_reps[0] == ',')
-                            _double_str_reps.erase(0, 1);
-                     }
-                }
+                if (!trim_delimeter_on_start(_double_str_reps) || _double_str_reps.size() == 0)
+                    continue;
 
                 if (_double_str_reps.size() == 0)
                     continue;
+
                 const double parsed_double = std::stod(_double_str_reps);
                 values.push_back(parsed_double);
             }
@@ -374,18 +587,47 @@ void try_parse_shp_prj(CString _inputStrConfig, SHPConfigPrj& data)
 int main()
 {
     SHPConfigPrj parsed_data;
+	
+    //типы данных arcgis и точность
+    //https://pro.arcgis.com/ru/pro-app/latest/help/data/geodatabases/overview/arcgis-field-data-types.htm
+
+    //- если секция имеет вид UNIT3[\"Kilometer\",], то секции будет задан флаг простой
     //- все численные представления парсятся как double
     //- строка конфига должна быть без пробелов, иначе какие-то значения могут считаться неверно
 
-    CString testStr = "GEOGCS[\"GCS_WGS_1984\",PRIMEM[\"Greenwich\",0.0]]";
+    //CString testStr = "GEOGCS[\"GCS_WGS_1984\",PRIMEM[\"Greenwich\",0.0]]";
     //CString testStr = "GEOGCS[\"GCS_WGS_1984\",UNIT[\"T\",10.0],PARAMETER[\"False_Easting\",500000.0]]";
     //CString testStr = "GEOGCS[\"GCS_WGS_1984\",DATUM[\"D_WGS_1984\",SPHEROID[\"WGS_1984\",6378137.000000,298.257224]],PRIMEM[\"Greenwich\",0.0],UNIT[\"Kilometer\",1000.0]]";
-    //CString testStr = "GEOGCS[\"GCS_Pulkovo_1942\",DATUM[\"D_Pulkovo_1942\",SPHEROID[\"Krasovsky_1940\",6378245.0,298.3]],PRIMEM[\"Greenwich\",0.0],UNIT[\"Degree\",0.0174532925199433]]";
+    CString testStr = "GEOGCS[\"GCS_Pulkovo_1942\",DATUM[\"D_Pulkovo_1942\",SPHEROID[\"Krasovsky_1940\",6378245.0,298.3]],PRIMEM[\"Greenwich\",0.0],UNIT[\"Degree\",0.0174532925199433,,666.0010098,1.0]]";
     //CString testStr = "GEOGCS[\"GCS_WGS_1984\",UNIT1[\"Kilometer\",UNIT2[\"R\",UNIT3[\"Kilometer\",10.0,,-23.0,45,-90,,,90]]]]";
     //CString testStr = "GEOGCS[\"GCS_WGS_1984\",UNIT1[\"Kilometer\",UNIT2[\"Rr\",UNIT3[\"Kilometer\",10.0,,-23.0,45,-90]]],DATUM[\"D_WGS_1984\",SPHEROID[\"WGS_1984\",6378137.000000,298.257224]],UNIT[\"Kilometer\",1000.0],PARAMETER2[\"Central_Meridian_Test\",-124.5],PROJECTION[\"Lambert_Conformal_Conic\"],PRIMEM[\"Greenwich\",0.0],PROJECTION2[\"Lambert_Conformal_Conic_Test\"],PROJECTION[\"Lambert_Conformal_Conic\"]]";
     //CString testStr = "GEOGCS[\"GCS_WGS_1984\",PROJECTION[\"Lambert_Conformal_Conic\"],PRIMEM[\"Greenwich2\",0.0]]";
 
     try_parse_shp_prj(testStr, parsed_data);
+
+    parsed_data.update_section_value_by_section_name("SPHEROID", "D_ITRF_2008");
+    std::vector<double> values_for_test;
+
+    //values_for_test.push_back(-1.00);
+    //values_for_test.push_back(21.00);
+    //values_for_test.push_back(1.23458891456789565456654543);
+    //values_for_test.push_back(-1.0010592323288999643499953);
+    //values_for_test.push_back(-100.0010592323288999643499953);
+
+    //values_for_test.push_back(12);
+    //values_for_test.push_back(-0.298);
+    //parsed_data.update_values_by_section_name("SPHEROID", values_for_test);
+
+    //values_for_test.clear();
+    //values_for_test.push_back(6378245.0);
+    //values_for_test.push_back(298.3);
+    //parsed_data.update_values_by_section_name("SPHEROID", values_for_test);
+
+    //values_for_test.clear();
+    //values_for_test.push_back(178.32);
+    //parsed_data.update_values_by_section_name("PRIMEM", values_for_test);
+
+    parsed_data.update_section_value_by_index("SPHEROID", -0.1234222, 1);
 
     return 0;
 }
